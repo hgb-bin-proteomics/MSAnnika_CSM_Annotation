e3d2a744ca6147e9e2a1e55e2fe433c8f4a1fa10
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-03-23 16:49:51 +0100

    Add doublet annotation, fix #1

--- get_intensities.py
+++ get_intensities.py
@@ -6,8 +6,8 @@
 # micha.birklbauer@gmail.com
 
 # version tracking
-__version = "1.0.3"
-__date = "2023-02-26"
+__version = "1.1.0"
+__date = "2023-03-23"
 
 # REQUIREMENTS
 # pip install pandas
@@ -19,6 +19,7 @@ __date = "2023-02-26"
 
 SPECTRA_FILE = "20220215_Eclipse_LC6_PepMap50cm-cartridge_mainlib_DSSO_3CV_stepHCD_OT_001.mgf"
 CSMS_FILE = "CSMs_unfiltered.xlsx"
+DOUBLETS_FILE = "CrosslinkDoublets.txt"
 MODIFICATIONS = \
     {"Oxidation": [15.994915],
      "Carbamidomethyl": [57.021464],
@@ -26,10 +27,14 @@ MODIFICATIONS = \
 ION_TYPES = ("b", "y")
 MAX_CHARGE = 4
 MATCH_TOLERANCE = 0.02
+DOUBLET_IDENTIFICATION_MODE = "All"
+# DOUBLET_IDENTIFICATION_MODE = "Evidence"
+# DOUBLET_IDENTIFICATION_MODE = "Indication"
 
 ######################
 
 # import packages
+import os
 import pandas as pd
 from tqdm import tqdm
 from pyteomics import mgf, mass
@@ -45,8 +50,8 @@ def read_spectra(filename: str) -> Dict[int, Dict]:
     """
     Returns a dictionary that maps scan numbers to spectra:
     Dict[int -> Dict["precursor" -> float
-                     "charge" -> int
-                     "peaks" -> Dict[m/z -> intensity]]
+                     "charge"    -> int
+                     "peaks"     -> Dict[m/z -> intensity]]
     """
 
     result_dict = dict()
@@ -63,6 +68,80 @@ def read_spectra(filename: str) -> Dict[int, Dict]:
             spectrum_dict["peaks"] = peaks
             result_dict[scan_nr] = spectrum_dict
         reader.close()
+
+    return result_dict
+
+def read_doublets(filename: str) -> Dict[int, Dict]:
+    """
+    Returns a dictionary that maps scan numbers to doublets:
+    Dict[int -> Dict["alpha_light" -> set(float)
+                     "alpha_heavy" -> set(float)
+                     "beta_light"  -> set(float)
+                     "beta_heavy"  -> set(float)]
+    """
+
+    proton_mass = 1.007276466812
+    result_dict = dict()
+    doublet_df = pd.read_csv(filename, sep = "\t")
+
+    for i, row in tqdm(doublet_df.iterrows(), total = doublet_df.shape[0], desc = "INFO: Progress bar - Reading doublet file"):
+
+        if DOUBLET_IDENTIFICATION_MODE != "All":
+            if DOUBLET_IDENTIFICATION_MODE not in row["Identification Mode"]:
+                continue
+
+        alpha_doublets = row["Complete Alpha Doublet"]
+        uncharged_aL, uncharged_aH = [float(m.strip()) for m in alpha_doublets.split("|")]
+        mz_aL = row["m/z Alpha Light"]
+        mz_aH = row["m/z Alpha Heavy"]
+        beta_doublets = row["Complete Beta Doublet"]
+        uncharged_bL, uncharged_bH = [float(m.strip()) for m in beta_doublets.split("|")]
+        mz_bL = row["m/z Beta Light"]
+        mz_bH = row["m/z Beta Heavy"]
+        scan_nr = row["Scan number"]
+
+        # alpha light possible m/z
+        aL_masses = set()
+        if mz_aL != 0:
+            aL_masses.add(mz_aL)
+
+        for charge in range(1, MAX_CHARGE + 1):
+            aL_masses.add((uncharged_aL + charge * proton_mass) / charge)
+
+        # alpha heavy possible m/z
+        aH_masses = set()
+        if mz_aH != 0:
+            aH_masses.add(mz_aH)
+
+        for charge in range(1, MAX_CHARGE + 1):
+            aH_masses.add((uncharged_aH + charge * proton_mass) / charge)
+
+        # beta light possible m/z
+        bL_masses = set()
+        if mz_bL != 0:
+            bL_masses.add(mz_bL)
+
+        for charge in range(1, MAX_CHARGE + 1):
+            bL_masses.add((uncharged_bL + charge * proton_mass) / charge)
+
+        # beta heavy possible m/z
+        bH_masses = set()
+        if mz_bH != 0:
+            bH_masses.add(mz_bH)
+
+        for charge in range(1, MAX_CHARGE + 1):
+            bH_masses.add((uncharged_bH + charge * proton_mass) / charge)
+
+        if scan_nr not in result_dict:
+            result_dict[scan_nr] = {"alpha_light": aL_masses,
+                                    "alpha_heavy": aH_masses,
+                                    "beta_light": bL_masses,
+                                    "beta_heavy": bH_masses}
+        else:
+            result_dict[scan_nr]["alpha_light"].update(aL_masses)
+            result_dict[scan_nr]["alpha_heavy"].update(aH_masses)
+            result_dict[scan_nr]["beta_light"].update(bL_masses)
+            result_dict[scan_nr]["beta_heavy"].update(bH_masses)
 
     return result_dict
 
@@ -184,9 +263,94 @@ def get_intensities(row: pd.Series, alpha: bool, spectra: Dict[int, Dict]) -> Tu
 
     return total_intensity, matched_fragments, theoretical_fragments
 
+def get_doublets(row: pd.Series, spectra: Dict[int, Dict], doublets: Dict[int, Dict]) -> Tuple[float,
+                                                                                               float,
+                                                                                               float,
+                                                                                               float,
+                                                                                               List[Tuple],
+                                                                                               List[Tuple],
+                                                                                               List[Tuple],
+                                                                                               List[Tuple]]:
+    """
+    Returns the sum of intensities of alpha light doublet peaks, alpha heavy doublet peaks, beta light doublet peaks, beta heavy doublet peaks (index 0 - 3).
+    Returns the peaks (Tuple (m/z, intensity)) of alpha light doublet peaks, alpha heavy doublet peaks, beta light doublet peaks, beta heavy doublet peaks (index 4 - 7).
+    """
+
+    scan_nr = row["First Scan"]
+    spectrum = spectra[scan_nr]
+    aL_doublets = []
+    aH_doublets = []
+    bL_doublets = []
+    bH_doublets = []
+
+    if scan_nr not in doublets:
+        return 0, 0, 0, 0, [], [], [], []
+    else:
+        for peak_mz in spectrum["peaks"].keys():
+
+            for doublet_mz in doublets[scan_nr]["alpha_light"]:
+                if round(peak_mz, 4) < round(doublet_mz + MATCH_TOLERANCE, 4) and round(peak_mz, 4) > round(doublet_mz - MATCH_TOLERANCE, 4):
+                    aL_doublets.append((peak_mz, spectrum["peaks"][peak_mz]))
+                    break
+
+            for doublet_mz in doublets[scan_nr]["alpha_heavy"]:
+                if round(peak_mz, 4) < round(doublet_mz + MATCH_TOLERANCE, 4) and round(peak_mz, 4) > round(doublet_mz - MATCH_TOLERANCE, 4):
+                    aH_doublets.append((peak_mz, spectrum["peaks"][peak_mz]))
+                    break
+
+            for doublet_mz in doublets[scan_nr]["beta_light"]:
+                if round(peak_mz, 4) < round(doublet_mz + MATCH_TOLERANCE, 4) and round(peak_mz, 4) > round(doublet_mz - MATCH_TOLERANCE, 4):
+                    bL_doublets.append((peak_mz, spectrum["peaks"][peak_mz]))
+                    break
+
+            for doublet_mz in doublets[scan_nr]["beta_heavy"]:
+                if round(peak_mz, 4) < round(doublet_mz + MATCH_TOLERANCE, 4) and round(peak_mz, 4) > round(doublet_mz - MATCH_TOLERANCE, 4):
+                    bH_doublets.append((peak_mz, spectrum["peaks"][peak_mz]))
+                    break
+
+        return sum([p[1] for p in aL_doublets]), \
+               sum([p[1] for p in aH_doublets]), \
+               sum([p[1] for p in bL_doublets]), \
+               sum([p[1] for p in bH_doublets]), \
+               aL_doublets, aH_doublets, bL_doublets, bH_doublets
+
+def get_total_fragment_intensity(row: pd.Series) -> float:
+    """
+    Returns the total intensity of fragment ions in the spectrum.
+    """
+
+    if row["Sequence A"].strip() == row["Sequence B"].strip():
+        return (row["Fragment Intensities A (Sum)"] + row["Fragment Intensities B (Sum)"]) / 2
+    else:
+        return row["Fragment Intensities A (Sum)"] + row["Fragment Intensities B (Sum)"]
+
+def get_total_doublet_intensity(row: pd.Series) -> float:
+    """
+    Returns the total intensity of doublet peaks in the spectrum.
+    """
+
+    if row["Sequence A"].strip() == row["Sequence B"].strip():
+        return (row["Alpha Doublet Intensities Total"] + row["Beta Doublet Intensities Total"]) / 2
+    else:
+        return row["Alpha Doublet Intensities Total"] + row["Beta Doublet Intensities Total"]
+
+def get_spectrum_intensity(row: pd.Series, spectra: Dict[int, Dict]) -> float:
+    """
+    Returns the total intensity of all peaks in a spectrum.
+    """
+
+    scan_nr = row["First Scan"]
+    spectrum = spectra[scan_nr]
+
+    total_intensity = 0
+    for peak_mz in spectrum["peaks"]:
+        total_intensity += spectrum["peaks"][peak_mz]
+
+    return total_intensity
+
 def main() -> pd.DataFrame:
 
-    print("INFO: Running CSMS annotation with input files:\nSpectra: " + SPECTRA_FILE + "\nCSMs: " + CSMS_FILE)
+    print("INFO: Running CSM annotation with input files:\nSpectra: " + SPECTRA_FILE + "\nCSMs: " + CSMS_FILE + "\nDoublets: " + DOUBLETS_FILE)
     print("INFO: Using the following modifications:")
     print(MODIFICATIONS)
     print("INFO: Using the following ion types:")
@@ -202,23 +366,48 @@ def main() -> pd.DataFrame:
 
     print("INFO: Reading CSMs...")
     csms = pd.read_excel(CSMS_FILE)
-    print("INFO: Done reading CSMs!")
+    print("INFO: Done reading CSMs! Starting fragment ion annotation...")
 
-    tqdm.pandas(desc = "INFO: Progress bar - Alpha Peptides:")
+    tqdm.pandas(desc = "INFO: Progress bar - Annotating alpha peptide fragments")
     csms[["Fragment Intensities A (Sum)", "Matched Ions A", "Theoretical Ions A"]] = \
         csms.progress_apply(lambda row: get_intensities(row, True, spectra),
-        axis = 1,
-        result_type = "expand")
-    print("INFO: Done processing Alpha Peptides!")
+                            axis = 1,
+                            result_type = "expand")
+    print("INFO: Done processing alpha peptides!")
 
-    tqdm.pandas(desc = "INFO: Progress bar - Beta Peptides:")
+    tqdm.pandas(desc = "INFO: Progress bar - Annotating beta peptide fragments")
     csms[["Fragment Intensities B (Sum)", "Matched Ions B", "Theoretical Ions B"]] = \
         csms.progress_apply(lambda row: get_intensities(row, False, spectra),
-        axis = 1,
-        result_type = "expand")
-    print("INFO: Done processing Beta Peptides!")
+                            axis = 1,
+                            result_type = "expand")
+    print("INFO: Done processing beta peptides!")
 
-    csms["Fragment Intensities Total"] = csms.apply(lambda row: row["Fragment Intensities A (Sum)"] + row["Fragment Intensities B (Sum)"], axis = 1)
+    csms["Fragment Intensities Total"] = csms.apply(lambda row: get_total_fragment_intensity(row), axis = 1)
+
+    print("INFO: Done annotating fragment ions!")
+
+    if DOUBLETS_FILE is not None and os.path.isfile(DOUBLETS_FILE):
+        print("INFO: Doublet file was provided! Reading doublet file...")
+        doublets = read_doublets(DOUBLETS_FILE)
+        print("INFO: Done reading doublet file! Starting doublet annotation...")
+        tqdm.pandas(desc = "INFO: Progress bar - Annotating doublets")
+        csms[["Alpha Light Intensities (Sum)", "Alpha Heavy Intensities (Sum)", "Beta Light Intensities (Sum)", "Beta Heavy Intensities (Sum)",
+              "Alpha Light Peaks", "Alpha Heavy Peaks", "Beta Light Peaks", "Beta Heavy Peaks"]] = \
+              csms.progress_apply(lambda row: get_doublets(row, spectra, doublets),
+                                  axis = 1,
+                                  result_type = "expand")
+        print("INFO: Done annotating doublets! Calculating intensities...")
+        csms["Alpha Doublet Intensities Total"] = csms.apply(lambda row: row["Alpha Light Intensities (Sum)"] + row["Alpha Heavy Intensities (Sum)"], axis = 1)
+        csms["Beta Doublet Intensities Total"] = csms.apply(lambda row: row["Beta Light Intensities (Sum)"] + row["Beta Heavy Intensities (Sum)"], axis = 1)
+        csms["Doublet Intensities Total"] = csms.apply(lambda row: get_total_doublet_intensity(row), axis = 1)
+        print("INFO: Done calculating doublet intensities!")
+    else:
+        print("INFO: Doublet file was not provided or not found! Skipping doublet annotation.")
+
+    print("INFO: Calculating total spectrum intensities...")
+    tqdm.pandas(desc = "INFO: Progress bar - Intensity per spectrum")
+    csms["Total Intensity in Spectrum"] = csms.progress_apply(lambda row: get_spectrum_intensity(row, spectra), axis = 1)
+    print("INFO: Done calculating total spectrum intensities!")
 
     csms.to_excel(".".join(CSMS_FILE.split(".")[:-1]) + "_with_intensities.xlsx")
     print("SUCCESS: Output file generated as '" + ".".join(CSMS_FILE.split(".")[:-1]) + "_with_intensities.xlsx" + "'!")
